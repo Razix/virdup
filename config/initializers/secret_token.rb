6527f3e344be98fe0824be096c4e5254deebf977
Razix <uscreamrazix@gmail.com>
2013-10-10 23:09:49 +0300

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Virdup::Application.config.secret_token = '53b8c4f413d6f69c29f6fdc62dd78b599cea965bd2a241044469b787fe4125fc8959fe4fd62e7704468b873042f10513b4ebacbeb0ef643985423c884998f844'
